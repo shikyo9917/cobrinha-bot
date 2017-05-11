0a4bf9a0cd83cd0a81304c5cd89c7651a143e18a
victor <freirel.victor@hotmail.com>
2017-05-11 15:12:25 +0000

    base

---
+++ main.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import manager
+import discord
+import logging
+class cobrinha(commands.Bot):
+    async def on_ready(self):
+        print('Logged in as')
+        print("Username: "+bots.user.name)
+        print("ID: "+bots.user.id)
+        print('==========\n')
+        for extension in manager.extensions:
+            print(f"  Loading {extension}...")
+            bots.load_extension("commands." + extension)
+            print(f"  Loading of module {extension} completed.")
+        print("Extension loading complete.\n")
+
+if __name__ == '__main__':
+    bots = cobrinha(command_prefix='~')
+    bots.run('')
